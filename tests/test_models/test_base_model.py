7d19cd2661c492d7d575df5c1711b8e1ac25edbe
Ashdof <mzigah4@gmail.com>
2023-02-07 16:44:32 +0000

    Compliance checks for pylint

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,18 +8,18 @@ from models.base_model import BaseModel
 
 class TestBaseModel(TestCase):
     """Class that inherits from testcase class"""
-    
+
     def test_initialization(self):
         """Tests constructor of class"""
-        b1 = BaseModel()
-        b1.name = "My First Model"
-        b1.number = 89
-        self.assertIs(type(b1), BaseModel)
-        self.assertEqual(b1.name, "My First Model")
-        self.assertEqual(b1.number, 89)
+        base = BaseModel()
+        base.name = "My First Model"
+        base.number = 89
+        self.assertIs(type(base), BaseModel)
+        self.assertEqual(base.name, "My First Model")
+        self.assertEqual(base.number, 89)
 
     def test_str_representation(self):
         """Tests string representation of instance of class"""
-        b2 = BaseModel()
-        b2_string = "[BaseModel] ({}) {}".format(b2.id, b2.__dict__)
-        self.assertEqual(b2_string, str(b2))
+        base_1 = BaseModel()
+        base_string = f"[BaseModel] ({base_1.id}) {base_1.__dict__}"
+        self.assertEqual(base_string, str(base_1))
