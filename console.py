153d0b42cf9e56860e4025857324ea93e371c996
Ashdof <mzigah4@gmail.com>
2023-02-09 16:01:03 +0000

    Removed the create method and improved the documentation comments

--- console.py
+++ console.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python3
-"""Contains the entry point of the command interpreter"""
-
+"""Command line interface implementation"""
 
 import cmd
 from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    """CLass for command line interface"""
+    """Class for command line interface"""
 
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Quits console upon entering of prompt"""
+        """Quits console
+
+        Description:
+            This method exits the console upon entering of prompt
+
+        Returns:
+            True upon exit
+        """
+
         return True
 
     def do_EOF(self, line):
-        """Exits console upon entering of prompt"""
+        """Quits console
+
+        Description:
+            This method is similar to the do_quit method. While this
+            method is inbuilt, the do_quit is the custom implementation
+            of this method
+        """
+
         return True
 
     def emptyline(self):
+        """Pass
+
+        Description:
+            This method overrides the inbuilt emptyline method and
+            executes nothing when nothing is typed. When the enter key
+            is pressed, the cursor jumps to the next line
         """
-        overrides emptyline method and executes nothing
-        when nothing is typed
-        """
+
         pass
 
-    def do_create(self, line):
-        """Creates a new instance of BaseModel"""
-        if line:
-            if line != "BaseModel":
-                print("** class doesn't exist **")
-            else:
-                inst = BaseModel()
-                inst.save()
-                print(inst.id)
-        else:
-            print("** class name missing **")
 
+#   ======================================================================
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
