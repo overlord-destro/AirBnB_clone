511a8b1e7e41274ea28c2dd024b43f312d4f78f9
overlord-destro <edemcoffie41@gmail.com>
2023-02-10 20:10:34 +0000

    Added class amenities

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class that stores info on amenities"""
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity that inherits from BaseModel"""
+    name = ""
