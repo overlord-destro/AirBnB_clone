5918de207e3d5f1b3a64da848595eb40bd6e2154
Ashdof <mzigah4@gmail.com>
2023-02-13 12:33:22 +0000

    Added import for all other classes in file_storage class. ALso, added import in console for all other classes and updated class variables with names of newly imported classes

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import shlex
 
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 
 
@@ -14,7 +19,12 @@ class HBNBCommand(cmd.Cmd):
 
     objects = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
             }
 
     prompt = "(hbnb) "
